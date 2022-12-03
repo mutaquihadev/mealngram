ee2718f9aea531a600704a11ac95b70c71dad8ef
mutaquihadev <olivio.mutaquiha@gmail.com>
2022-12-03 18:25:47 +0100

    refactor progress calculation

--- app/src/main/java/dk/kriaactividade/mealngram/presentation/recipeList/RecipeListFragment.kt
+++ app/src/main/java/dk/kriaactividade/mealngram/presentation/recipeList/RecipeListFragment.kt
@@ -1,7 +1,5 @@
 package dk.kriaactividade.mealngram.presentation.recipeList
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -11,8 +9,7 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.chip.Chip
@@ -22,9 +19,9 @@ import dk.kriaactividade.mealngram.data.domain.Recipe
 import dk.kriaactividade.mealngram.database.room.RoomViewModel
 import dk.kriaactividade.mealngram.databinding.FragmentRecipeListBinding
 import dk.kriaactividade.mealngram.databinding.LayoutBottonSheetDialogBinding
-import dk.kriaactividade.mealngram.presentation.utils.Constants.RESULT_FROM_DETAILS
 import dk.kriaactividade.mealngram.presentation.utils.gone
 import dk.kriaactividade.mealngram.presentation.utils.visible
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -38,108 +35,130 @@ class RecipeListFragment : Fragment() {
     @Inject
     lateinit var roomViewModel: RoomViewModel
     private val recipeListAdapter by lazy {
-        RecipeListAdapter(requireContext(), ::getDetailsRecipes, recipesViewModel::updateChipState)
+        RecipeListAdapter(requireContext(), ::getDetailsRecipes, recipesViewModel::onChipSelected)
     }
     private var listDetails = mutableListOf<DetailsRecipes>()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentRecipeListBinding.inflate(layoutInflater)
         activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        setupObservers()
-        setupAdapter()
-        binding.fabAdd.setOnClickListener { recipesViewModel.updateEditMode() }
-        binding.buttonOk.setOnClickListener {
-            val currentBackStackEntry = findNavController().currentBackStackEntry
-            val savedStateHandle = currentBackStackEntry?.savedStateHandle
-            savedStateHandle?.getLiveData<Boolean>(RESULT_FROM_DETAILS)
-                ?.observe(currentBackStackEntry, Observer { result ->
-                    recipesViewModel.clearSelectionMode(result)
-                })
-            findNavController().navigate(RecipeListFragmentDirections.goToMyRecipes(listDetails.toTypedArray()))
+        //setupObservers()
+        //setupAdapter()
+//        binding.fabAdd.setOnClickListener { recipesViewModel.updateEditMode() }
+//        binding.buttonOk.setOnClickListener {
+//            val currentBackStackEntry = findNavController().currentBackStackEntry
+//            val savedStateHandle = currentBackStackEntry?.savedStateHandle
+//            savedStateHandle?.getLiveData<Boolean>(RESULT_FROM_DETAILS)
+//                ?.observe(currentBackStackEntry, Observer { result ->
+//                    recipesViewModel.clearSelectionMode(result)
+//                })
+//            findNavController().navigate(RecipeListFragmentDirections.goToMyRecipes(listDetails.toTypedArray()))
+//        }
+        binding.recyclerHome.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = recipeListAdapter
         }
 
+        binding.fabAdd.setOnClickListener { recipesViewModel.updateEditMode() }
 
+        lifecycleScope.launch {
+            recipesViewModel.uiState.collect { uiState ->
+                when (uiState) {
+                    RecipeListUiState.Error -> {
+
+                    }
+                    RecipeListUiState.Loading -> binding.loading.visible()
+
+                    is RecipeListUiState.Success -> {
+                        binding.loading.gone()
+                        binding.layoutRecipes.visible()
+                        recipeListAdapter.submitList(uiState.uiData.recipes)
+
+                        binding.progress.isVisible = uiState.uiData.showProgress
+                        binding.progress.progress = uiState.uiData.progressValue
+                        binding.buttonOk.isVisible = uiState.uiData.showButton
+
+                    }
+                }
+            }
+        }
 
         return binding.root
     }
 
     private fun setupObservers() {
-        observerProgress()
-        observerRecipes()
-        observerEditMode()
-        observerButton()
-        observerDetailsList()
+//        observerProgress()
+//        observerRecipes()
+//        observerEditMode()
+//        observerButton()
+//        observerDetailsList()
     }
 
     private fun observerEditMode() {
-        recipesViewModel.isEditMode.observe(viewLifecycleOwner) { isEditMode ->
-            animateFab(binding.fabAdd, isEditMode)
-            binding.progress.isVisible = isEditMode
-        }
+//        recipesViewModel.isEditMode.observe(viewLifecycleOwner) { isEditMode ->
+//            animateFab(binding.fabAdd, isEditMode)
+//            binding.progress.isVisible = isEditMode
+//        }
     }
 
-    private fun observerRecipes() {
-        recipesViewModel.recipes.observe(viewLifecycleOwner) {
-            if (it.isNotEmpty()) {
-                recipeListAdapter.submitList(it)
-                binding.apply {
-                    loading.gone()
-                    layoutRecipes.visible()
-                }
-            }
-            saveInCache(it)
-        }
-    }
-
-    private fun saveInCache(recipeList: List<Recipe>) {
-        roomViewModel.allPerson.observe(viewLifecycleOwner) {
-            if (it.isEmpty()) {
-                roomViewModel.insertList(recipeList)
-            }
-        }
-
-    }
-
-    private fun observerProgress() {
-        recipesViewModel.valueProgress.observe(viewLifecycleOwner) {
-            binding.progress.progress = it
-        }
-    }
-
-    private fun observerButton() {
-        recipesViewModel.showButton.observe(viewLifecycleOwner) {
-            binding.buttonOk.isVisible = it
-        }
-    }
-
-    private fun observerDetailsList() {
-        recipesViewModel.addDetailsRecipes.observe(viewLifecycleOwner) { details ->
-            listDetails.add(details)
-        }
-    }
-
-    private fun animateFab(v: View, rotate: Boolean) {
-        v.animate().setDuration(200)
-            .setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    super.onAnimationEnd(animation)
-                }
-            })
-            .rotation(if (rotate) 135f else 0f)
-    }
-
-
-    private fun setupAdapter() {
-        binding.recyclerHome.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            adapter = recipeListAdapter
-        }
-    }
-
+//    private fun observerRecipes() {
+//        recipesViewModel.recipes.observe(viewLifecycleOwner) {
+//            if (it.isNotEmpty()) {
+//                recipeListAdapter.submitList(it)
+//                binding.apply {
+//                    loading.gone()
+//                    layoutRecipes.visible()
+//                }
+//            }
+//            saveInCache(it)
+//        }
+//    }
+//
+//    private fun saveInCache(recipeList: List<Recipe>) {
+//        roomViewModel.allPerson.observe(viewLifecycleOwner) {
+//            if (it.isEmpty()) {
+//                roomViewModel.insertList(recipeList)
+//            }
+//        }
+//
+//    }
+//
+//    private fun observerProgress() {
+//        recipesViewModel.valueProgress.observe(viewLifecycleOwner) {
+//            binding.progress.progress = it
+//        }
+//    }
+//
+//    private fun observerButton() {
+//        recipesViewModel.showButton.observe(viewLifecycleOwner) {
+//            binding.buttonOk.isVisible = it
+//        }
+//    }
+//
+//    private fun observerDetailsList() {
+//        recipesViewModel.addDetailsRecipes.observe(viewLifecycleOwner) { details ->
+//            listDetails.add(details)
+//        }
+//    }
+//
+//    private fun animateFab(v: View, rotate: Boolean) {
+//        v.animate().setDuration(200).setListener(object : AnimatorListenerAdapter() {
+//                override fun onAnimationEnd(animation: Animator) {
+//                    super.onAnimationEnd(animation)
+//                }
+//            }).rotation(if (rotate) 135f else 0f)
+//    }
+//
+//
+//    private fun setupAdapter() {
+//        binding.recyclerHome.apply {
+//            layoutManager = LinearLayoutManager(requireContext())
+//            adapter = recipeListAdapter
+//        }
+//    }
+//
     private fun getDetailsRecipes(recipes: Recipe) {
         val dialog = BottomSheetDialog(requireContext())
         val binding = LayoutBottonSheetDialogBinding.inflate(layoutInflater)
@@ -155,10 +174,11 @@ class RecipeListFragment : Fragment() {
             }
             val listImage = mutableListOf<String>()
             listImage.add(recipes.image)
-            vpImageRecipes.adapter =
-                DetailsRecipesViewPageAdapter(requireContext(), listImage)
+            vpImageRecipes.adapter = DetailsRecipesViewPageAdapter(requireContext(), listImage)
             indicator.setupWithViewPager(vpImageRecipes, true)
         }
         dialog.show()
     }
 }
+
+
--- app/src/main/java/dk/kriaactividade/mealngram/presentation/recipeList/RecipeListViewModel.kt
+++ app/src/main/java/dk/kriaactividade/mealngram/presentation/recipeList/RecipeListViewModel.kt
@@ -1,24 +1,42 @@
 package dk.kriaactividade.mealngram.presentation.recipeList
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import dk.kriaactividade.mealngram.data.domain.*
+import dk.kriaactividade.mealngram.data.domain.ChipState
+import dk.kriaactividade.mealngram.data.domain.Recipe
+import dk.kriaactividade.mealngram.data.domain.WEEK
 import dk.kriaactividade.mealngram.data.repository.RecipesRepository
 import dk.kriaactividade.mealngram.helpers.DataState
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.*
 import javax.inject.Inject
 import kotlin.math.roundToInt
 
-class RecipeListViewModel @Inject constructor(private val repository: RecipesRepository) :
-    ViewModel() {
-    private var countProgress = 0
-    private val selectedChipStates = mutableListOf(
+data class RecipeListUiData(
+    val showButton: Boolean = false,
+    val showProgress: Boolean = false,
+    val progressValue: Int = 0,
+    val recipes: List<Recipe>
+)
+
+sealed interface RecipeListUiState {
+    object Loading : RecipeListUiState
+    object Error : RecipeListUiState
+    data class Success(val uiData: RecipeListUiData) : RecipeListUiState
+}
+
+class RecipeListViewModel @Inject constructor(repository: RecipesRepository) : ViewModel() {
+
+    private var isEditMode: Boolean = false
+
+    private val _uiState: MutableStateFlow<RecipeListUiState> =
+        MutableStateFlow(RecipeListUiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
+    private val recipes = mutableListOf<Recipe>()
+
+    private val selectedChipStates = listOf(
         SelectedChip(WEEK.MONDAY),
         SelectedChip(WEEK.TUESDAY),
         SelectedChip(WEEK.WEDNESDAY),
@@ -28,89 +46,29 @@ class RecipeListViewModel @Inject constructor(private val repository: RecipesRep
         SelectedChip(WEEK.SUNDAY)
     )
 
-    val recipes: LiveData<List<Recipe>>
-        get() = _recipes
-    private val _recipes = MutableLiveData<List<Recipe>>()
-
-    val addDetailsRecipes: LiveData<DetailsRecipes>
-        get() = _addDetailsRecipes
-    private val _addDetailsRecipes = MutableLiveData<DetailsRecipes>()
-
-    val valueProgress: LiveData<Int>
-        get() = _valueProgress
-    private val _valueProgress = MutableLiveData<Int>()
-
-    val showButton: LiveData<Boolean>
-        get() = _showButton
-    private val _showButton = MutableLiveData<Boolean>()
-
-    val isEditMode: LiveData<Boolean>
-        get() = _isEditMode
-    private val _isEditMode = MutableLiveData<Boolean>(false)
-
     init {
-        viewModelScope.launch { repository.getAllRecipes().collect(::handleGetAllRecipesResult) }
+        viewModelScope.launch {
+            repository.getAllRecipes().collect(::handleGetAllRecipes)
+        }
     }
 
-    private fun handleGetAllRecipesResult(state: DataState<List<Recipe>>) {
-        when(state) {
+    private fun handleGetAllRecipes(state: DataState<List<Recipe>>) {
+        when (state) {
+            is DataState.Error -> {}
             is DataState.Data -> {
-                val recipes = state.data
-                _recipes.postValue(recipes)
+                recipes.clear()
+                recipes.addAll(state.data)
+                _uiState.value =
+                    RecipeListUiState.Success(uiData = RecipeListUiData(recipes = recipes))
             }
-            is DataState.Error -> {
-
-            }
-            is DataState.Loading -> {
-
-            }
+            is DataState.Loading -> _uiState.value = RecipeListUiState.Loading
         }
-    }
-
-    private fun getValueProgress(value: Boolean) {
-        val valueProgress: Double = 10.0 / 0.7 + 1
-        val valueInt = valueProgress.roundToInt()
-        if (value) {
-            countProgress += valueInt
-            _valueProgress.postValue(countProgress)
-            if (countProgress > 100) {
-                _showButton.postValue(true)
-            }
-        } else {
-            countProgress -= valueInt
-            _valueProgress.postValue(countProgress)
-            if (countProgress < 100) {
-                _showButton.postValue(false)
-            }
-        }
-    }
-
-    private fun hideButton() {
-        _showButton.postValue(false)
-    }
-
-    private fun clearProgress() {
-        _valueProgress.postValue(0)
-        countProgress = 0
     }
 
     fun updateEditMode() {
-        isEditMode.value?.let { currentEditMode ->
-            val updatedEditMode = !currentEditMode
-            _isEditMode.postValue(updatedEditMode)
+        isEditMode = !isEditMode
 
-            updateRecipes(updatedEditMode)
-        }
-    }
-
-    fun clearSelectionMode(clear:Boolean){
-       updateRecipes(!clear)
-        _isEditMode.postValue(!clear)
-    }
-
-    private fun updateRecipes(isSelectionMode: Boolean = true) {
-        val recipes = _recipes.value?.map { recipe ->
-
+        val updatedRecipes = recipes.map { recipe ->
             Recipe(
                 id = recipe.id,
                 name = recipe.name,
@@ -119,54 +77,53 @@ class RecipeListViewModel @Inject constructor(private val repository: RecipesRep
                 image = recipe.image,
                 video = recipe.video,
                 mainImage = recipe.mainImage,
-                isSelectionMode = isSelectionMode
+                isSelectionMode = isEditMode
             )
-        } ?: emptyList()
+        }
 
-        clearSelectedRecipes()
-        hideButton()
-        clearProgress()
-        _recipes.postValue(recipes)
+        _uiState.value = RecipeListUiState.Success(
+            uiData = RecipeListUiData(recipes = updatedRecipes, showProgress = isEditMode)
+        )
+
+        if(!isEditMode) {
+            clearSelectedRecipes()
+            clearSelectionMode()
+        }
     }
+
+    private fun clearSelectionMode() {
+        val clearedRecipes = recipes.map { recipe ->
+            Recipe(
+                id = recipe.id,
+                name = recipe.name,
+                description = recipe.description,
+                ingredients = recipe.ingredients,
+                image = recipe.image,
+                video = recipe.video,
+                mainImage = recipe.mainImage,
+                isSelectionMode = false,
+            )
+        }
+
+        recipes.clear()
+        recipes.addAll(clearedRecipes)
+    }
+
 
     private fun clearSelectedRecipes() {
         selectedChipStates.forEach { it.recipeId = null }
     }
 
-    fun updateChipState(recipeId: Int, weekDay: WEEK, selectedState: Boolean) {
+    fun onChipSelected(recipeId: Int, weekDay: WEEK, selectedState: Boolean) {
         val updatedSelectedState = !selectedState
-        getValueProgress(updatedSelectedState)
 
         selectedChipStates.first { it.weekDay == weekDay }.recipeId =
             if (updatedSelectedState) recipeId else null
 
-        val recipes = _recipes.value?.map { recipe ->
+        val updatedRecipes = recipes.map { recipe ->
 
             val updatedChipStates = selectedChipStates.mapIndexed { index, selectedChipState ->
-
-                val recipeChipState = recipe.dayOfWeekSelectedPair[index]
-                selectedChipState.recipeId?.let { selectedId ->
-
-                    if (selectedId == recipe.id) {
-                        createDetailsList(recipe, weekDay)
-                        ChipState(
-                            id = recipeChipState.id,
-                            isActive = !recipeChipState.isActive,
-                            isVisible = true,
-                            dayOfWeek = recipeChipState.dayOfWeek
-                        )
-
-                    } else {
-                        ChipState(
-                            id = recipeChipState.id,
-                            isActive = false,
-                            isVisible = false,
-                            dayOfWeek = recipeChipState.dayOfWeek
-                        )
-
-                    }
-
-                } ?: ChipState(id = recipeChipState.id, dayOfWeek = recipeChipState.dayOfWeek)
+                updateChipStates(recipe, index, selectedChipState)
             }
 
             Recipe(
@@ -180,62 +137,52 @@ class RecipeListViewModel @Inject constructor(private val repository: RecipesRep
                 isSelectionMode = true,
                 dayOfWeekSelectedPair = updatedChipStates
             )
-        } ?: emptyList()
-
-        _recipes.postValue(recipes)
-    }
-
-    private fun getDay(day: Int): String {
-        val dateFormat: DateFormat = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
-        val calendar = Calendar.getInstance()
-        calendar.firstDayOfWeek = Calendar.SUNDAY
-        val dayWeek = calendar[Calendar.DAY_OF_WEEK]
-        when (day) {
-            0 -> {
-                calendar.add(Calendar.DAY_OF_MONTH, Calendar.MONDAY - dayWeek)
-            }
-            1 -> {
-                calendar.add(Calendar.DAY_OF_MONTH, Calendar.TUESDAY - dayWeek)
-            }
-            2 -> {
-                calendar.add(Calendar.DAY_OF_MONTH, Calendar.WEDNESDAY - dayWeek)
-            }
-            3 -> {
-                calendar.add(Calendar.DAY_OF_MONTH, Calendar.THURSDAY - dayWeek)
-            }
-            4 -> {
-                calendar.add(Calendar.DAY_OF_MONTH, Calendar.FRIDAY - dayWeek)
-            }
-            5 -> {
-                calendar.add(Calendar.DAY_OF_MONTH, Calendar.SATURDAY - dayWeek)
-            }
-            6 -> {
-                calendar.add(Calendar.DAY_OF_MONTH, Calendar.SUNDAY - dayWeek)
-            }
         }
-        return dateFormat.format(calendar.time)
-    }
 
-    private fun createDetailsList(
-        recipe: Recipe,
-        dayOfWeek: WEEK
-    ) {
-        val details = DetailsRecipes(
-            id = recipe.id,
-            name = recipe.name,
-            description = recipe.description,
-            image = recipe.image,
-            dayOfWeek = dayOfWeek,
-            day = setDay(dayOfWeek),
-            ingredients = recipe.ingredients
+        recipes.clear()
+        recipes.addAll(updatedRecipes)
+
+        val progressValue = ((selectedChipStates.filter { it.recipeId != null }.size.toFloat() / selectedChipStates.size.toFloat()) * 100).roundToInt()
+        val showButton = progressValue == 100
+
+        _uiState.value = RecipeListUiState.Success(
+            uiData = RecipeListUiData(
+                recipes = recipes,
+                showProgress = true,
+                showButton = showButton,
+                progressValue = progressValue
+            )
         )
-        _addDetailsRecipes.postValue(details)
     }
 
-    private fun setDay(dayOfWeek: WEEK): String {
-        return getDay(dayOfWeek.id)
+    private fun updateChipStates(
+        recipe: Recipe,
+        index: Int,
+        selectedChipState: SelectedChip,
+    ): ChipState {
+        val recipeChipState = recipe.dayOfWeekSelectedPair[index]
+        return selectedChipState.recipeId?.let { selectedId ->
+
+            if (selectedId == recipe.id) {
+                ChipState(
+                    id = recipeChipState.id,
+                    isActive = !recipeChipState.isActive,
+                    isVisible = true,
+                    dayOfWeek = recipeChipState.dayOfWeek
+                )
+
+            } else {
+                ChipState(
+                    id = recipeChipState.id,
+                    isActive = false,
+                    isVisible = false,
+                    dayOfWeek = recipeChipState.dayOfWeek
+                )
+
+            }
+
+        } ?: ChipState(id = recipeChipState.id, dayOfWeek = recipeChipState.dayOfWeek)
     }
 }
-
 
 data class SelectedChip(val weekDay: WEEK, var recipeId: Int? = null)
